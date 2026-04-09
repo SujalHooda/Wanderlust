0a4e54e962fa42471ec1f8799f2115f87d69a5f6
Sujal Hooda <sujalhooda6@gmail.com>
2026-04-10 03:55:46 +0530

    navbar created

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const mongoose = require('mongoose');
 const Listing = require('./models/listing.js');
 const path = require('path');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
@@ -65,7 +68,15 @@ app.put("/listings/:id", async(req,res) => {
     let {id} = req.params;
     const listing = await Listing.findByIdAndUpdate(id, {...req.body.listing});
     res.redirect(`/listings/${id}`);
-})
+});
+
+// Delete route
+app.delete("/listings/:id", async(req,res) => {
+    let {id} = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log("Deleted listing: ", deletedListing);
+    res.redirect("/listings");
+});
 
 // app.get("/testlisting", async(req,res) => {
 //     let sampleListing = new Listing({
